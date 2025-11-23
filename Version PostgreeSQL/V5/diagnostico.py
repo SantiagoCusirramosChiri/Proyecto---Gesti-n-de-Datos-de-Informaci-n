e55e96e5ab7f1d5f89d5f169bbea9e132ab3e358
SantiagoCusirramosChiri <santiago.cusirramos@ucsm.edu.pe>
2025-11-22 19:25:07 -0500

    se actualiza el control de versiones para la v5

--- Version PostgreeSQL/V5/diagnostico.py
+++ Version PostgreeSQL/V5/diagnostico.py
@@ -2,15 +2,24 @@
 import os
 import glob
 
-postgres_dir = r"C:\Program Files\PostgreSQL\18"
+# Buscar en todas las versiones de PostgreSQL
+postgres_base_dir = r"C:\Program Files\PostgreSQL"
 
 print("Buscando archivos .conf con encoding incorrecto...\n")
 
 archivos_conf = []
-for root, dirs, files in os.walk(postgres_dir):
-    for file in files:
-        if file.endswith(('.conf', '.conf.sample')):
-            archivos_conf.append(os.path.join(root, file))
+# Buscar en todas las versiones de PostgreSQL
+if os.path.exists(postgres_base_dir):
+    for version_dir in os.listdir(postgres_base_dir):
+        full_version_path = os.path.join(postgres_base_dir, version_dir)
+        if os.path.isdir(full_version_path):
+            for root, dirs, files in os.walk(full_version_path):
+                for file in files:
+                    if file.endswith(('.conf', '.conf.sample')):
+                        archivos_conf.append(os.path.join(root, file))
+else:
+    print("✗ No se encontró el directorio de PostgreSQL")
+    exit(1)
 
 for archivo in archivos_conf:
     try:
